62d437f0f9d4f7792e13c4bdcf88e6cfcd8346e4
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2020-03-25 12:18:52 +0100

    add basic ccall test for flint (malloc+free)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using LoadFlint
 using Test
+using Libdl
 
 # Just successfully loading LoadFlint is a good first test
 
-# TODO: add some trivial ccall's into flint
+@testset "LoadFlint" begin
+    @test occursin("libgmp", LoadFlint.libgmp)
+    @test occursin("libflint", LoadFlint.libflint)
+
+    l = dllist()
+    @test length(filter(x->occursin("libflint", x), l)) == 1
+    x = Ptr{Nothing}(0)
+    @test (x = ccall((:flint_malloc,LoadFlint.libflint), Ptr{Nothing}, (UInt,), 8)) != C_NULL
+    @test ccall((:flint_free,LoadFlint.libflint), Nothing ,(Ptr{Nothing},),x) == nothing
+
+    # TODO maybe test fmpz?
+end
